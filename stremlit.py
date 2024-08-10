974eabd0cbaef6a6f6a3d67f1ec0e848ca2c92b9
NaifGHannam <dhdfau@gmail.com>
2024-08-10 11:46:31 +0300

    upd  2

--- stremlit.py
+++ stremlit.py
@@ -1,15 +1,30 @@
 import streamlit as st
 
 # Set the app title
-st.set_page_config(page_title="My Streamlit App")
+st.set_page_config(page_title="employment")
 
 # Add a title
-st.title("Welcome to My Streamlit App")
+st.title(" وظائف حديثين التخرج في سوق العمل السعودي ")
+
+# Add a statement
+statement = "  كحديث تخرج يبحث عن عمل  كان عندي فضول كم نسبة الوظائف اللي تطلب  حديث تخرج و  وما هي اكثر مدينه فيها وظائف عملت تحليل لبيانات منصة جدارة و كانت اعلى نسبة توظيف في الرياض بفرق شاسع عن باقي مدن المملكه وتليها مدينة جدة حيث انه تقريبا 50% من الوظائف في هذه المدينتين و تتشارك باقي مدن المملكه في باقي  النسبه "
+st.write(statement)
+
+image = "cities.png"
+st.image(image, caption="نسبة اعلانات التوظيف في مدن المملكه ")
+# Add a statement
+statement = "كانت الرواتب تتراوح في حدود 5803 ريال حيث ان اكثر راتب كان 8803 و اقل راتب 3000  "
+st.write(statement)
 
 # Add an image
 image = "range.png"
-st.image(image, caption=" range image ")
+st.image(image, caption="رواتب حديثين التخرج ")
 
 # Add a statement
-statement = "This is a statement about my Streamlit app."
+statement = "ما يقارب 60% من اعلانات التوظيف تستهدف حديثين التخرج "
 st.write(statement)
+
+# Add an image
+image = "نسبة الطلب.png"
+st.image(image, caption="نسبة التوظيف  على حسب سنوات الخبره ")
+
